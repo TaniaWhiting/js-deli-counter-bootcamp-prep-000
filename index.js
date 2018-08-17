2ec161fa85ce0c37c51f0f66763f02977f96edc1
Tania Whiting <tanwhiting@aol.com>
2018-08-17 03:45:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function nowServing(katzDeliLine){
   if (katzDeliLine.length > 0) {
     var name = katzDeliLine[0];
     katzDeliLine.shift;
-    return name;
+    return (name);
   } else {
     return("There is nobody waiting to be served!");
   }
