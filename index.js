c7f92013d4ba7a98121eb0b791285c7c78f63c5f
Tania Whiting <tanwhiting@aol.com>
2018-08-17 04:23:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ function takeANumber(katzDeliLine, name){
 }
 
 function nowServing(katzDeliLine){
-  if (katzDeliLine.length > 0) {
-    var name = katzDeliLine[0];
-    katzDeliLine.shift();
-    return ("Currently serving " + name + ".");
-  } else {
-    return("There is nobody waiting to be served!");
+  for (var i = 0; i < katzDeliLine.length; i += 1) {
+    if (katzDeliLine.length === 0) {
+    return "There is nobody waiting to be served!";
+    } else {
+      return ('Currently serving ' + katzDeliLine.shift() + ".");
+    }
   }
 }
 
